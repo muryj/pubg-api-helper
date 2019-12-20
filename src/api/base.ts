f9215d2c05946ff8c9fa1e25add54b161216708f
Yevhenii <Muryi>
2019-12-20 18:50:45 +0200

    fix class methods

--- src/api/base.ts
+++ src/api/base.ts
@@ -7,7 +7,7 @@ export class APIHelper {
     this._apiKey = apiKey;
     this._platform = platform;
   }
-  baseRequest(params: string) {
+  public baseRequest(params: string) {
     return fetch(`https://api.playbattlegrounds.com/shards/${this._platform}/${params}`, {
       method: 'GET',
       headers: {
@@ -16,13 +16,13 @@ export class APIHelper {
       },
     });
   }
-  playerByName(playerName: string): object {
+  public playerByName(playerName: string): object {
     return this.baseRequest(`players?filter[playerNames]=${playerName}`);
   }
-  playerMatchesByID(playerID: string): object {
+  public playerMatchesByID(playerID: string): object {
     return this.baseRequest(`matches/${playerID}`);
   }
-  playerLifeTimeStats(playerID: string): object {
+  public playerLifeTimeStats(playerID: string): object {
     return this.baseRequest(`players/${playerID}/seasons/lifetime`);
   }
 }
