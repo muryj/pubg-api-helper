7e15fa95948698d934e8be02e6c49d00def31e80
Yevhenii <Muryi>
2019-12-21 15:52:29 +0200

    remove invalid tests

--- src/__tests__/Test.test.ts
+++
@@ -1,8 +0,0 @@
-import { APIHelper, Platform } from '../index';
-
-jest.mock('../api/base.ts');
-
-it('constructor called', () => {
-  const api = new APIHelper('test', Platform.STEAM);
-  expect(api).toHaveBeenCalledTimes(1);
-});
